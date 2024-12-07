3ee9ce3d0e6225ee42f14a941451e992ce63c8de
Dmitri9149 <dmitri.as.name@gmail.com>
2024-12-07 18:22:46 +0200

    get number of breeds in DB

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,10 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
 import styles from '@/app/ui/home.module.css';
+import {fetchBreedsQuantity} from '@/app/lib/data';
  
 export default async function Page() {
+  const breedsQuantity = await fetchBreedsQuantity();
 
   return (
     <main>
@@ -11,7 +13,7 @@ export default async function Page() {
         Dashboard
       </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Total Breeds" value={8} type="breeds" />
+        <Card title="Total Breeds" value={breedsQuantity.toString()} type="breeds" />
         <Card
           title="Total Customers"
           value={1}
