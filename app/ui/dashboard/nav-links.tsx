83ae9c67e38bc986e78a70b67ecce5f024a2da67
Dmitri9149 <dmitri.as.name@gmail.com>
2024-12-07 14:29:43 +0200

    change the name of nav links buttons

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,11 +14,11 @@ import clsx from 'clsx';
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Traits',
-    href: '/dashboard/traits',
+    name: 'Breeds',
+    href: '/dashboard/breeds',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Breeds', href: '/dashboard/breeds', icon: UserGroupIcon },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
