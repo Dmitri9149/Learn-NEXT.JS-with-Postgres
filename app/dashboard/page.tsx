ceac33469840058f3404a0ccc6d07aa92d9b377e
Dmitri9149 <dmitri.as.name@gmail.com>
2024-12-07 14:19:31 +0200

    cards for 'total breeds' and 'total customers'

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,32 +1,22 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import {Traits, Scroll}  from '@/app/ui/dashboard/traits';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchTraits } from '@/app/lib/data';
 import styles from '@/app/ui/home.module.css';
  
 export default async function Page() {
-  const traits = await fetchTraits();
 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className={styles.shape}>
-        {/*      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"> */}
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Total Breeds" value={8} type="breeds" />
+        <Card
           title="Total Customers"
-          value={numberOfCustomers}
+          value={1}
           type="customers"
-        /> */}
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
-        {<Traits traits={traits} />}
+        />
       </div>
     </main>
   );
