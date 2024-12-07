c6ff233a294183b5e8b88ea0f837249daea6d79f
Dmitri9149 <dmitri.as.name@gmail.com>
2024-12-07 14:30:16 +0200

    change the name of folders and url paths

--- app/dashboard/breeds/page.tsx
+++ app/dashboard/breeds/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <p>Breeds Page</p>;
+import {Traits, Scroll}  from '@/app/ui/dashboard/traits';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchTraits } from '@/app/lib/data';
+import styles from '@/app/ui/home.module.css';
+ 
+export default async function Page() {
+  const traits = await fetchTraits();
+
+  return (
+    <main>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {<Traits traits={traits} />}
+      </div>
+    </main>
+  );
 }
